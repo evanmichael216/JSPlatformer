55e1abf762495e9dba614e3f3f9890059b781c47
evanmichael216 <evanmichael216@knights.ucf.edu>
2016-10-13 15:32:07 -0400

    First Commit
    Initial files for platformer.

---
+++ code/levels.js
@@ -0,0 +1,30 @@
+var GAME_LEVELS = [
+  ["                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                   xx      xx      !            ",
+   "                                    o o      xx                   !!!           ",
+   "                                                       o           !            ",
+   "              y                    xxxxx                                        ",
+   "              y         o o                            y                        ",
+   "              y                           o o          y                        ",
+   "                       xxxxx                           y                     x  ",
+   "                                         xxxxx              y              o x  ",
+   "             xxxx                                           y                x  ",
+   "     @       x  x                                           o    xxxxx       x  ",
+   "  xxxxxxxxxxxx  xxxxxxxxxxxxxxx                            xxxxxxx   xxxxxxxxx  ",
+   "                              x!!!!!!!!!!!!!!!!!!!!!!!!!!!!x                    ",
+   "                              x!!!!!!!!!!!!!!!!!!!!!!!!!!!!x                    ",
+   "                              x!!!!!!!!!!!!!!!!!!!!!!!!!!!!x                    ",
+   "                              xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                    ",
+   "                                                                                ",
+   "                                                                                "],
+
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = GAME_LEVELS;
